c34ffe046d2d55068f2bbc28a59df2a7509608c1
danielmacapa <71198712+danielmacapa@users.noreply.github.com>
2022-05-11 18:14:29 -0300

    migrations1

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\UsersController;
 
 Route::get('/users', [UsersController::class, 'index'])->name('users.index');
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/users/{id}', [UsersController::class, 'show'])->name('users.show');
+
+//Rota Padrão do Laravel
+Route::get('/', function () { return view('welcome'); });
